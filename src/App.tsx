8e213dd4aedc1a9d11e5169326cc7361ea3f3a69
Hung Nguyen <hung.nguyen@aventragroup.com>
2023-03-17 16:17:50 +0700

    Add error handling + app providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 // import './App.css';
-import { QueryClientProvider } from 'react-query';
 
 import { Counter } from './features/counter';
-import { queryClient } from './lib/react-query';
+import { AppProvider } from './providers/app';
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Counter />
-    </QueryClientProvider>
+    <AppProvider>
+      {/* <AppRoutes /> */} <Counter></Counter>
+    </AppProvider>
   );
 }
 
